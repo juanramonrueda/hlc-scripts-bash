d2c29dc7ba53854df75e69f939a52a731d1f9907
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-02-14 12:29:45 +0100

    Se realizan adiciones al código

--- tercera-relacion/script_3.sh
+++ tercera-relacion/script_3.sh
@@ -7,8 +7,14 @@
 # Se pasa el primer argumento dado a variable
 FICHERO_1="$1"
 
+# Variable para crear otro fichero
+FICHERO_1_1="$1_1"
+
 # Se pasa el segundo argumento dado a variable
 FICHERO_2="$2"
+
+# Variable para crear otro fichero
+FICHERO_2_1="$2_1"
 
 # Se pasa el tercer argumento dado a variable
 FICHERO_COMPARACION="$3"
@@ -34,7 +40,17 @@ function main() {
     # Llamada a la función que explica cómo funciana el script
     how_works_script
   else
-    diff --suppress-common-lines $FICHERO_1 $FICHERO_2 | tee -a $FICHERO_COMPARACION
+    # Se pasa el contenido del fichero a otro junto con las líneas numeradas
+    awk '{print NR, $0}' $FICHERO_1 > $FICHERO_1_1
+
+    # Se pasa el contenido del fichero a otro junto con las líneas numeradas
+    awk '{print NR, $0}' $FICHERO_2 > $FICHERO_2_1
+
+    # Se realiza la comprobación de las diferencias de los ficheros en el tercer fichero
+    diff --suppress-common-lines $FICHERO_1_1 $FICHERO_2_1 | tee -a $FICHERO_COMPARACION
+
+    # Borrado de los archivos intermedios para la numeración de las líneas
+    rm $FICHERO_1_1 && rm $FICHERO_2_1
   fi
 }
 
