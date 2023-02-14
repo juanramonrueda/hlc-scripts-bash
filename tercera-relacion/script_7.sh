18ba66fd952e63efb9a681b834ee0d1bec01f9d3
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-02-14 17:09:08 +0100

    Se finaliza el séptimo script

---
+++ tercera-relacion/script_7.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+#-----------------------------------------------------------------------------------
+# Declaración de función
+
+# Declaración de la función principal
+function main() {
+  # Se obtiene el formato de hora, minutos y segundos sin delimitadores en variable
+  HORA=$(date +%H%M%S)
+
+  # Si la hora se encuentra entre las 06:00:00 y las 12:59:59
+  if (( $HORA >= 060000 && $HORA < 130000 )); then
+    echo "Buenos días" 
+
+  # Si la hora se encuentra entre las 13:00:00 y las 19:59:59
+  elif (( $HORA >= 130000 && $HORA < 200000 )) ; then
+    echo "Buenas tardes"
+  
+  # En el resto de los casos
+  else
+    echo "Buenas noches"
+  fi
+}
+
+
+#-----------------------------------------------------------------------------------
+# Ejecución de la función principal
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
