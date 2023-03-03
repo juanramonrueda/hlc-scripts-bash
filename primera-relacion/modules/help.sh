cae1b508df15755d281287689a895ab2eb843b55
Juan Ramón Rueda Lao <juanramonrueda.94@gmail.com>
2023-03-03 17:55:20 +0100

    Update help.sh

--- primera-relacion/modules/help.sh
+++ primera-relacion/modules/help.sh
@@ -2,11 +2,17 @@
 
 
 #---------------------------------------------------------------------------------------------------------------------
-# Declaración de función
+# Declaración de funciones
 
 # Función para mostrar la ayuda del script
 function ayuda() {
   echo -e "\e[38;5;162mEste script tiene varias funciones disponibles al ejecutarlo sin argumentos\e[0m" && echo ""
   opciones_disponibles
   exit 0
+}
+
+# Se muestra un aviso para el usuario de que puede necesitarse permisos de sudo
+function aviso_sudo() {
+  echo -e "\e[1;38;5;196mEste script puede necesitar permisos de sudo para que algunas funcionalidades se ejecuten correctamente\e[0m"
+  echo ""
 }
