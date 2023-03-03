56aca3aa56f7fb1c2c5d7d74a832fd9e395a7662
Juan Ramón Rueda Lao <juanramonrueda.94@gmail.com>
2023-03-03 17:52:54 +0100

    Update help.sh

--- primera-relacion/modules/help.sh
+++ primera-relacion/modules/help.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-#----------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------
 # Declaración de función
 
 # Función para mostrar la ayuda del script
 function ayuda() {
-  echo -e "\e[38;5;162mEste script tiene varias funciones disponibles\e[0m" && echo ""
+  echo -e "\e[38;5;162mEste script tiene varias funciones disponibles al ejecutarlo sin argumentos\e[0m" && echo ""
   opciones_disponibles
   exit 0
 }
