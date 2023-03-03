40655674ffc649f7ac53da3a5371957a6e198614
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-03-03 13:04:41 +0100

    Se agrega una ayuda mediante -h o --help

--- escaneo-redes/escanear.sh
+++ escaneo-redes/escanear.sh
@@ -4,8 +4,11 @@
 #---------------------------------------------------------------------------------------------------------------------------
 # Declararación de variables
 
-# Conversión del primer argumento recibido a variable
-NET_ADDRESS="${1}"
+# Conversión del primer argumento recibido a variable y se cambia el scope a "global"
+export FIRST_ARG="${1}"
+
+# Patrón para verificar si el argumento pasado es una IP
+PATRON_IP="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"
 
 # Contador para conocer los hosts activos
 CONTADOR_ACTIVOS=0
@@ -14,7 +17,7 @@ CONTADOR_ACTIVOS=0
 DIRECTORIO_ESCANEO="nmap_txt"
 
 # Nombre del archivo
-NOMBRE_FICHERO="Red-${NET_ADDRESS}.txt"
+NOMBRE_FICHERO="Red-${FIRST_ARG}.txt"
 
 # Obtención del usuario que ejecuta el script para obtener su directorio personal
 RUTA_DIRECTORIO_USUARIO=$( eval echo "~${USUARIO}" )
@@ -28,51 +31,66 @@ RUTA_COMPLETA_FICHERO="${RUTA_DIRECTORIO_FICHEROS}/${NOMBRE_FICHERO}"
 
 #---------------------------------------------------------------------------------------------------------------------------
 # Declaración de función
-function main() {
-  # Llamada a la función para limpiar la pantalla
-  source modules/clear_screen.sh
 
+# Función para obtener los hosts activos en la red designada por argumento
+function main() {
   # Comprobación de la cantidad de argumentos pasados
-  if (( $# != 1 )); then
-    # Llamada al script que contiene la función ayuda
+  if (( ${#} == 1 )); then
+    # Comprobación del uso de la ayuda
+    if [[ ${FIRST_ARG} == "-h" || ${FIRST_ARG} == "--help" ]]; then
+      # Llamada al script que contiene la función ayuda
+      source modules/help.sh
+
+    else
+      # Comprobación del formato del argumento recibido
+      if [[ ${FIRST_ARG} =~ ${PATRON_IP} ]]; then
+        # Comprobación de que el directorio para los archivos exista
+        if [[ ! -d ${RUTA_DIRECTORIO_FICHEROS} ]]; then
+          mkdir ${RUTA_DIRECTORIO_FICHEROS}
+        fi
+
+        # Obtención de la IP sin el último octeto
+        NO_ZERO_IP=$( echo "${FIRST_ARG}" | awk -F '.' '{print $1,$2,$3}' OFS='.' )
+
+        # Bucle para recorrer toda la red y hacer ping a las IPs
+        for CUARTO_OCTETO in $(seq 1 1 254)
+        do
+          # Progreso del escaneo, no se guarda en el archivo
+          echo "Pinging to ${NO_ZERO_IP}.${CUARTO_OCTETO}"
+          
+          # Ping a la IP que está en ese momento en el bucle y su salida se manda a NULL
+          ping -c 1 "${NO_ZERO_IP}.${CUARTO_OCTETO}" >/dev/null 2>&1
+
+          # Obtención del RC del comando para saber si ha sido exitoso el ping o no
+          RESULTADO=$( echo $? )
+
+          # Comparación del resultado del ping para saber si el host estaba UP or DOWN
+          if (( ${RESULTADO} == 0 )); then
+            # Se usa DATE para saber la hora a la que se realiza el ping y se pasa por pipeline a TEE -A para añadir al fichero
+            echo "($(date "+%F %H:%M:%S")) El host ${NO_ZERO_IP}.${CUARTO_OCTETO} está activo" | tee -a ${RUTA_COMPLETA_FICHERO}
+
+            # Incremento de la variable contadora para saber cuántos hosts están activos
+            let "CONTADOR_ACTIVOS++"
+          fi
+        done
+
+        # Mensaje de que el escaneo de la red ha finalizado
+        echo "Escaneo finalizado" && echo ""
+
+        # Mensaje de cuántos hosts estaban activos al momento de realizar el escaneo de la red
+        echo "Se han encontrado ${CONTADOR_ACTIVOS} host activos"
+      else
+        echo "El argumento pasado no es una IP o no tiene el formato correcto"
+        source modules/help.sh
+        exit 1
+      fi
+    fi
+  else
+    # Llamada al script que hace de ayuda
     source modules/help.sh
 
-  else
-    # Comprobación de que el directorio para los archivos exista
-    if [[ ! -d ${RUTA_DIRECTORIO_FICHEROS} ]]; then
-      mkdir ${RUTA_DIRECTORIO_FICHEROS}
-    fi
-
-    # Obtención de la IP sin el último octeto
-    NO_ZERO_IP=$( echo "${NET_ADDRESS}" | cut -d "." -f 1,2,3 )
-
-    # Bucle para recorrer toda la red y hacer ping a las IPs
-    for CUARTO_OCTETO in $(seq 1 1 254)
-    do
-      # Progreso del escaneo, no se guarda en el archivo
-      echo "Pinging to ${NO_ZERO_IP}.${CUARTO_OCTETO}"
-      
-      # Ping a la IP que está en ese momento en el bucle y su salida se manda a NULL
-      ping -c 1 "${NO_ZERO_IP}.${CUARTO_OCTETO}" >/dev/null 2>&1
-
-      # Obtención del RC del comando para saber si ha sido exitoso el ping o no
-      RESULTADO=$( echo $? )
-
-      # Comparación del resultado del ping para saber si el host estaba UP or DOWN
-      if [[ ${RESULTADO} -eq 0 ]]; then
-        # Se usa DATE para saber la hora a la que se realiza el ping y se pasa por pipeline a TEE -A para añadir al fichero
-        echo "($(date +%H:%M:%S)) El host ${NO_ZERO_IP}.${CUARTO_OCTETO} está activo" | tee -a ${RUTA_COMPLETA_FICHERO}
-
-        # Incremento de la variable contadora para saber cuántos hosts están activos
-        let "CONTADOR_ACTIVOS++"
-      fi
-    done
-
-    # Mensaje de que el escaneo de la red ha finalizado
-    echo "Escaneo finalizado" && echo ""
-
-    # Mensaje de cuántos hosts estaban activos al momento de realizar el escaneo de la red
-    echo "Se han encontrado ${CONTADOR_ACTIVOS} host activos"
+    # Salida con código de error en el caso de que el argumento no sea correcto
+    exit 1
   fi
 }
 
@@ -81,5 +99,5 @@ function main() {
 # Ejecución de la función principal
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "${@}"
+  main "${@}"
 fi
--- escaneo-redes/modules/help.sh
+++ escaneo-redes/modules/help.sh
@@ -6,10 +6,9 @@
 
 # Función principal para indicar cómo funciona el script
 function main() {
-  echo -e "\e[38;5;198mPara usar correctamente el script, tiene que pasar una dirección de red o una IP completa:"
-  echo "" && echo -e "\e[1m${0} 192.168.1.0" && echo "" && echo -e "${0} 192.168.1.22\e[0m"
-
-  exit 1
+  echo -e "\e[38;5;162mEste script tiene la función de comprobar los hosts activos en la red"
+  echo -e "Para usar el script, tiene que pasar una dirección de red o una IP de host\e[0m"
+  echo "" && echo -e "\e[38;5;129m${0} 192.168.1.0" && echo -e "${0} 192.168.1.10\e[0m"
 }
 
 
