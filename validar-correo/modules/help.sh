3b7b811e0f1f689507ff460059741836318707c4
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-03-03 19:20:22 +0100

    Se reestructura la ayuda

--- validar-correo/modules/help.sh
+++ validar-correo/modules/help.sh
@@ -2,20 +2,21 @@
 
 
 #-------------------------------------------------------------------------------------------------------------------------
-# Declaración de función
+# Declaración de funciones
 
 # Función para explicar al usuario cómo funciona el script
-function main() {
-  echo -e "\e[1;38;5;124mEste script se encarga de validar correos electrónicos"
-  echo "Para hacer uso del script, pase un correo electrónico como argumento para hacer su validación:"
-  echo -e "${0} correo@ejemplo.net"
-
-  # Salida no exitosa
-  exit 1
+function ayuda() {
+  echo -e "\e[38;5;162mEste script se encarga de la validación de correos electrónicos"
+  echo "Para hacer uso del script, pase un correo electrónico como argumento para hacer su validación:\e[0m"
+  echo -e "\e[38;5;129m${0} <correo_electrónico>\e[0m"
 }
 
 
-#-------------------------------------------------------------------------------------------------------------------------
-# Ejecución de la función principal
+# Función para indicar que no se han pasado la cantidad de argumentos requeridos
+function error_arg() {
+  echo -e "\e[38;5;196mNo ha pasado la cantidad de argumentos necesaria"
+  echo -e "Debe pasar un único argumento:\e[0m"
+  echo -e "\e[38;5;129m${0} <correo_electrónico>\e[0m"
+  echo "" && echo -e "\e[38;5;162mPuede visualizar la ayuda con ${0} -h ó ${0} --help\e[0m"
 
-main
+}
--- validar-correo/validacion_correo.sh
+++ validar-correo/validacion_correo.sh
@@ -7,7 +7,7 @@
 # Se convierte el primer argumento a variable
 CORREO_A_VALIDAR="${1}"
 
-# Patrón que valida el correo electrónico
+# Patrón que valida el correo electrónico, permite uso de string, integer, dot, dash y hyphen
 PATRON_CORREO="^[a-zA-Z0-9._-]{1,64}@[a-zA-Z0-9.-]{4,255}\.[a-z]{2,4}$"
 
 
@@ -16,23 +16,30 @@ PATRON_CORREO="^[a-zA-Z0-9._-]{1,64}@[a-zA-Z0-9.-]{4,255}\.[a-z]{2,4}$"
 
 # Función principal
 function main() {
-  # Llamada al script que contiene la función de limpieza de pantalla
-  source modules/clear_screen.sh
+  # Importación del módulo que contiene la ayuda con respecto al script
+  source modules/help.sh
 
   # Comprobación de la cantidad de argumentos pasados al script
-  if (( ${#} != 1 )); then
-    # En el caso de que sean distintos de 1, ejecutará el script "ayuda"
-    source modules/help.sh
-  else
-    # Comprobación del correo pasado con el patrón definido al principio
-    if [[ ${CORREO_A_VALIDAR} =~ ${PATRON_CORREO} ]]; then
-      echo -e "\e[1;38;5;46mEl correo electrónico ${CORREO_A_VALIDAR} es un correo válido"
+  if (( ${#} == 1 )); then
+    if [[ ${1} == "-h" || ${1} == "--help" ]]; then
+      # Llamada a la función que explica el funcionamiento del script
+      ayuda
+    
     else
-      echo -e "\e[1;38;5;196mEl correo electrónico ${CORREO_A_VALIDAR} no es un correo válido"
+      # Comprobación del correo pasado con el patrón definido al principio
+      if [[ ${CORREO_A_VALIDAR} =~ ${PATRON_CORREO} ]]; then
+        echo -e "\e[1;38;5;46mEl correo electrónico ${CORREO_A_VALIDAR} es un correo válido"
+      else
+        echo -e "\e[1;38;5;196mEl correo electrónico ${CORREO_A_VALIDAR} no es un correo válido"
 
-      # Salida no exitosa al no pasar la validación
-      exit 1
+        # Salida no exitosa al no pasar la validación
+        exit 1
+      fi
     fi
+
+  else
+    # Llamada al script para indicar que no se ha pasado la cantidad de argumentos requerida
+    error_arg
   fi
 }
 
