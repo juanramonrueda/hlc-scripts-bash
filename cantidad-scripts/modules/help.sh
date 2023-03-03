8632d529f729ad55f9ebc5b97b359b5ba9e58d4a
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-03-03 12:59:23 +0100

    Se cambia la estructura

--- cantidad-scripts/modules/help.sh
+++ cantidad-scripts/modules/help.sh
@@ -5,9 +5,9 @@
 
 # Función para mostrar la ayuda del funcionamiento del script
 function main() {
-  echo -e "\e[38;5;162mEste script tiene la función de contar los scripts que tienen los usuarios\e[0m" && echo ""
-  echo -e "\e[38;5;129m${0} [usuario] <fichero_salida>\e[0m" && echo ""
-  echo -e "\e[38;5;162mPara usarlo, puede pasar opcionalmente un usuario (sin []) y obligatoriamente un fichero de salida (sin <>)"
+  echo -e "\e[38;5;162mEste script tiene la función de contar los scripts que tienen los usuarios"
+  echo -e "Para usarlo, puede pasar opcionalmente un usuario (sin []) y obligatoriamente un fichero de salida (sin <>)\e[0m"
+  echo "" && echo -e "\e[38;5;129m${0} [usuario] <fichero_salida>\e[0m" && echo ""
   echo "En el caso de que no pase un usuario, contará los scripts de cada usuario y el total de los mismos"
   echo -e "En el caso de que pase un usuario, contará únicamente los scripts de ese usuario\e[0m"
 
