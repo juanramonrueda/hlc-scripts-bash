68b5501681589173b2a473c464d73d5104a26e3e
Juan Ramón Rueda <juanramonrueda.94@gmail.com>
2023-03-04 12:05:56 +0100

    Se finaliza la ayuda del segundo script

--- tercera-relacion/script_2.sh
+++ tercera-relacion/script_2.sh
@@ -45,47 +45,47 @@ function file_doesnt_exists() {
 # Función principal
 function main() {
   # Llamada al script de limpieza de pantalla
-  source modules/clear_screen.sh
+  source modules/script_2/help.sh
 
   # Comprobación de la cantidad de argumentos pasados
-  if (( $# != 1 )); then
-    # Llamada a la función que explica qué hace el script
-    ayuda
+  if (( ${#} == 1 )); then
+    if [[ ${1} == "-h" || ${1} == "--help" ]]; then
+      # Llamada a la función que explica qué hace el script
+      ayuda
 
-    # Llamada al script que explica cómo funciona el script
-    source modules/how_works_script_1.sh
-
-  # Entrada a esta condición cuando el número de argumentos sea 1
-  else
-    # Comprobación de la no existencia del fichero
-    if ! test -f ${RUTA_FICHERO} ; then
-      echo -e "\e[91mEl fichero no existe" && echo "" && file_doesnt_exists
-
-    # Entrada a esta condición debido a la existencia del fichero en la ruta
     else
-      # Obtención del nombre del archivo en variable
-      NOMBRE_FICHERO=$( echo "${RUTA_FICHERO}" | rev | cut -d "/" -f 1 | rev )
-    
-      echo "El fichero ${NOMBRE_FICHERO} existe" && echo ""
-    
-      # Se usa el modificador -e para coger el backslash "\" como caracter normal y se añade un código de color, \e[94m
-      echo -e "\e[1;94mContenido del archivo con líneas numeradas\e[0;94m:"
-    
-      # Se muestra el contenido del fichero con las líneas numeradas y se pone el color por defecto mediante \e[0m
-      awk '{print NR, $0 }' ${RUTA_FICHERO} && echo -e "\e[0m"
+      # Comprobación de la no existencia del fichero
+      if ! test -f ${RUTA_FICHERO} ; then
+        echo -e "\e[38;5;196mEl fichero no existe" && echo "" && file_doesnt_exists
 
-      # Se obtienen la cantidad total de líneas en variable
-      CANTIDAD_LINEAS_FICHERO=$( wc -l < ${RUTA_FICHERO} )
-      echo -e "\e[1;92mEl fichero tiene ${CANTIDAD_LINEAS_FICHERO} líneas en total\e[0m" && echo ""
+      # Entrada a esta condición debido a la existencia del fichero en la ruta
+      else
+        # Obtención del nombre del archivo en variable
+        NOMBRE_FICHERO=$( echo "${RUTA_FICHERO}" | rev | awk -F '/' '{print $1}' | rev )
       
-      # Se obtienen la cantidad de palabras que tiene el fichero
-      CANTIDAD_PALABRAS_FICHERO=$( wc -w < ${RUTA_FICHERO} )
-      echo -e "\e[1;95mEl fichero tiene ${CANTIDAD_PALABRAS_FICHERO} palabras en total\e[0m" && echo ""
+        # Se usa el modificador -e para coger el backslash "\" como caracter normal y se añade un código de color, \e[94m
+        echo -e "\e[1;94mContenido del archivo con líneas numeradas\e[0;94m:"
+      
+        # Se muestra el contenido del fichero con las líneas numeradas y se pone el color por defecto mediante \e[0m
+        awk '{print NR, $0 }' ${RUTA_FICHERO} && echo -e "\e[0m"
 
-      # Se obtienen la cantidad de caracteres que tiene el fichero
-      CANTIDAD_CARACTERES_FICHERO=$( wc -m < ${RUTA_FICHERO} )
-      echo -e "\e[1;33mEl fichero tiene ${CANTIDAD_CARACTERES_FICHERO} caracteres en total\e[0m"
+        # Se obtienen la cantidad total de líneas en variable
+        CANTIDAD_LINEAS_FICHERO=$( wc -l < ${RUTA_FICHERO} )
+        echo -e "\e[1;92mEl fichero tiene ${CANTIDAD_LINEAS_FICHERO} líneas en total\e[0m" && echo ""
+        
+        # Se obtienen la cantidad de palabras que tiene el fichero
+        CANTIDAD_PALABRAS_FICHERO=$( wc -w < ${RUTA_FICHERO} )
+        echo -e "\e[1;95mEl fichero tiene ${CANTIDAD_PALABRAS_FICHERO} palabras en total\e[0m" && echo ""
+
+        # Se obtienen la cantidad de caracteres que tiene el fichero
+        CANTIDAD_CARACTERES_FICHERO=$( wc -m < ${RUTA_FICHERO} )
+        echo -e "\e[1;33mEl fichero tiene ${CANTIDAD_CARACTERES_FICHERO} caracteres en total\e[0m"
+      fi
     fi
+
+  # Entrada a esta condición cuando no se ha pasado ningún argumento
+  else
+    no_arg  
   fi
 }
 
@@ -94,5 +94,5 @@ function main() {
 # Ejecución de la función principal
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "${@}"
+  main "${@}"
 fi
